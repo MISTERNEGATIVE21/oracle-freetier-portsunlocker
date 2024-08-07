13f1a106cc7134cb9d1e53e51b8008f7fc73458c
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2024-08-07 13:33:16 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-# ASCII Art Function
+# Function to display ASCII art
 display_ascii_art() {
-    echo " ____                       _     _     _____          __   __           ____      "
-    echo "|  _ \                     | |   | |   |_   _|         \ \ / /          |  _ \     "
-    echo "| |_) |_ __ ___  _   _  ___| |__ | |_    | |    ___     \ V /___  _   _ | |_) |_   _"
-    echo "|  _ <| '__/ _ \| | | |/ __| '_ \| __|   | |   / _ \     \ // _ \| | | ||  _ <| | | |"
-    echo "| |_) | | | (_) | |_| | (__| | | | |_    | |  | (_) |    | | (_) | |_| || |_) | |_| |"
-    echo "|____/|_|  \___/ \__,_|\___|_| |_|\__|   |_|   \___/     \_/\___/ \__,_||____/ \__, |"
-    echo "                                                                                __/ |"
-    echo "                                                                               |___/ "
-    echo "              __  __ _____  _____ _______ ______ _____   _   _ ______ _____       _______ _____      _______ "
-    echo "             |  \/  |_   _|/ ____|__   __|  ____|  __ \ | \ | |  ____|  __ \   /\|__   __|_   _\ \   / / ____|"
-    echo "             | \  / | | | | (___    | |  | |__  | |__) ||  \| | |__  | |  | | /  \  | |    | |  \ \_/ / |  __   "
-    echo "             | |\/| | | |  \___ \   | |  |  __| |  _  / | . \` |  __| | |  | |/ /\ \ | |    | |   \   /| | |_ |"
-    echo "             | |  | |_| |_ ____) |  | |  | |____| | \ \ | |\  | |____| |__| / ____ \| |   _| |_   | | | |__| |"
-    echo "             |_|  |_|_____|_____/   |_|  |______|_|  \_\|_| \_|______|_____/_/    \_\_|  |_____|  |_|  \_____|"
+    cat << "EOF"
+ ____                       _     _     _____          __   __           ____      
+|  _ \                     | |   | |   |_   _|         \ \ / /          |  _ \     
+| |_) |_ __ ___  _   _  ___| |__ | |_    | |    ___     \ V /___  _   _ | |_) |_   _
+|  _ <| '__/ _ \| | | |/ __| '_ \| __|   | |   / _ \     \ // _ \| | | ||  _ <| | | |
+| |_) | | | (_) | |_| | (__| | | | |_    | |  | (_) |    | | (_) | |_| || |_) | |_| |
+|____/|_|  \___/ \__,_|\___|_| |_|\__|   |_|   \___/     \_/\___/ \__,_||____/ \__, |
+                                                                                __/ |
+                                                                               |___/ 
+              __  __ _____  _____ _______ ______ _____   _   _ ______ _____       _______ _____      _______ 
+             |  \/  |_   _|/ ____|__   __|  ____|  __ \ | \ | |  ____|  __ \   /\|__   __|_   _\ \   / / ____|
+             | \  / | | | | (___    | |  | |__  | |__) ||  \| | |__  | |  | | /  \  | |    | |  \ \_/ / |  __  
+             | |\/| | | |  \___ \   | |  |  __| |  _  / | . ` |  __| | |  | |/ /\ \ | |    | |   \   /| | |_ | 
+             | |  | |_| |_ ____) |  | |  | |____| | \ \ | |\  | |____| |__| / ____ \| |   _| |_   | | | |__| | 
+             |_|  |_|_____|_____/   |_|  |______|_|  \_\|_| \_|______|_____/_/    \_\_|  |_____|  |_|  \_____| 
+EOF
     echo
     echo
 }
 
-# Function to display the options
+# Function to display the menu options
 display_options() {
     echo "Web Hosting Panel Installation Options"
     echo "---------------------------------------"
@@ -33,38 +35,57 @@ display_options() {
     echo "7. Install Virtualmin"
     echo "8. Check System Requirements"
     echo "9. View Panel Documentation"
-    echo "10. Exit"
+    echo "10. Set Terminal"
+    echo "11. Reset Terminal"
+    echo "12. Exit"
     echo
 }
 
 # Function to install CloudPanel
 install_cloudpanel() {
     echo "Installing CloudPanel..."
-    curl -sSL https://installer.cloudpanel.io/ce/v2/install.sh | sudo bash
+    if curl -sSL https://installer.cloudpanel.io/ce/v2/install.sh | sudo bash; then
+        echo "CloudPanel installed successfully."
+    else
+        echo "Failed to install CloudPanel."
+    fi
+    pause
 }
 
 # Function to install CyberPanel
 install_cyberpanel() {
     echo "Installing CyberPanel..."
-    sh <(curl -s https://cyberpanel.net/install.sh || wget -q -O - https://cyberpanel.net/install.sh)
+    if sh <(curl -s https://cyberpanel.net/install.sh || wget -q -O - https://cyberpanel.net/install.sh); then
+        echo "CyberPanel installed successfully."
+    else
+        echo "Failed to install CyberPanel."
+    fi
+    pause
 }
 
 # Function to install CASA Panel
 install_casa() {
     echo "Installing CASA Panel..."
     echo "Please visit https://github.com/casaos/casaos for installation instructions."
+    pause
 }
 
 # Function to install Froxlor
 install_froxlor() {
     echo "Installing Froxlor..."
     echo "Please visit https://froxlor.org/install for installation instructions."
+    pause
 }
 
 # Function to install aaPanel
 install_aapanel() {
     echo "Installing aaPanel..."
-    wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh
+    if wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh; then
+        echo "aaPanel installed successfully."
+    else
+        echo "Failed to install aaPanel."
+    fi
+    pause
 }
 
 # Function to install Webmin
@@ -75,14 +96,23 @@ install_webmin() {
     wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] http://download.webmin.com/download/repository sarge contrib"
     sudo apt update
-    sudo apt install -y webmin
+    if sudo apt install -y webmin; then
+        echo "Webmin installed successfully."
+    else
+        echo "Failed to install Webmin."
+    fi
+    pause
 }
 
 # Function to install Virtualmin
 install_virtualmin() {
     echo "Installing Virtualmin..."
-    wget -O virtualmin-install.sh https://raw.githubusercontent.com/virtualmin/virtualmin-install/master/virtualmin-install.sh
-    sudo sh virtualmin-install.sh
+    if wget -O virtualmin-install.sh https://raw.githubusercontent.com/virtualmin/virtualmin-install/master/virtualmin-install.sh && sudo sh virtualmin-install.sh; then
+        echo "Virtualmin installed successfully."
+    else
+        echo "Failed to install Virtualmin."
+    fi
+    pause
 }
 
 # Function to check system requirements
@@ -95,6 +125,7 @@ check_requirements() {
     echo "- 10 GB Disk Space"
     echo "- 64-bit (x86_64) CPU"
     echo "- Ubuntu 20.04 LTS or later (for most panels)"
+    pause
 }
 
 # Function to view documentation
@@ -119,24 +150,62 @@ view_documentation() {
         7) xdg-open "https://www.virtualmin.com/documentation/" ;;
         *) echo "Invalid choice." ;;
     esac
+    pause
 }
 
-# Main script
-clear
-display_ascii_art
-display_options
-read -p "Enter your choice [1-10]: " choice
+# Function to pause and wait for user input
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
 
-case $choice in
-    1) install_cloudpanel ;;
-    2) install_cyberpanel ;;
-    3) install_casa ;;
-    4) install_froxlor ;;
-    5) install_aapanel ;;
-    6) install_webmin ;;
-    7) install_virtualmin ;;
-    8) check_requirements ;;
-    9) view_documentation ;;
-    10) echo "Exiting..."; exit 0 ;;
-    *) echo "Invalid option. Please run the script again and select a valid option." ;;
-esac
+# Function to set terminal
+set_terminal(){
+    echo "Setting terminal..."
+    # Add terminal setting commands here
+    pause
+}
+
+# Function to reset terminal
+reset_terminal(){
+    echo "Resetting terminal..."
+    # Add terminal reset commands here
+    pause
+}
+
+# Function to display menus
+show_menus() {
+    clear
+    display_ascii_art
+    display_options
+}
+
+# Function to read input from the keyboard and take action
+read_options(){
+    local choice
+    read -p "Enter choice [1-12]: " choice
+    case $choice in
+        1) install_cloudpanel ;;
+        2) install_cyberpanel ;;
+        3) install_casa ;;
+        4) install_froxlor ;;
+        5) install_aapanel ;;
+        6) install_webmin ;;
+        7) install_virtualmin ;;
+        8) check_requirements ;;
+        9) view_documentation ;;
+        10) set_terminal ;;
+        11) reset_terminal ;;
+        12) echo "Exiting..."; exit 0 ;;
+        *) echo "Error... Invalid option." && sleep 2
+    esac
+}
+
+# Trap CTRL+C, CTRL+Z and quit signals
+trap '' SIGINT SIGQUIT SIGTSTP
+
+# Main logic - infinite loop
+while true
+do
+    show_menus
+    read_options
+done
